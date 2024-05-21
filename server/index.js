b960d32285a18b447dcddde4bb38efb39bd3c7f6
maximmft <mmaufront@gmail.com>
2024-05-21 09:53:12 +0200

    quete 2 done

--- server/index.js
+++ server/index.js
@@ -12,6 +12,13 @@ const app = require("./app/config");
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
 
+// Define a simple route
+const sayHello = (req,res) => {
+  res.send("Welcome to Wild Series !");
+};
+
+app.get("/", sayHello);
+
 // Start the server and listen on the specified port
 app
   .listen(port, () => {
@@ -20,3 +27,4 @@ app
   .on("error", (err) => {
     console.error("Error:", err.message);
   });
+
