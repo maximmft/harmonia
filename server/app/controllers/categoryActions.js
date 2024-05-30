d8202deb6663566e0d20be7be88237b034e9f44c
maximmft <mmaufront@gmail.com>
2024-05-30 12:40:17 +0200

    quest done

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,41 +1,37 @@
+const tables = require("../../database/tables");
+
 // Some data to make the trick
 
 const categories = [
-    {
-      id: 1,
-      name: "Science-Fiction",
-    },
-    {
-      id: 2,
-      name: "Comédie",
-    },
-  ];
-  
-  // Declare the actions
-  
-  const browse = (req, res) => {
-    if (req.query.q != null) {
-      const filteredCategories = categories.filter((category) =>
-        category.name.includes(req.query.q)
-      );
-  
-      res.json(filteredCategories);
-    } else {
-      res.json(categories);
-    }
-  };
+  {
+    id: 1,
+    name: "Science-Fiction",
+  },
+  {
+    id: 2,
+    name: "Comédie",
+  },
+];
 
-  const read = (req, res) => {
-    const parsedId = parseInt(req.params.id, 10);
-  
-    const category = categories.find((p) => p.id === parsedId);
-  
-    if (category != null) {
-      res.json(category);
-    } else {
-      res.sendStatus(404);
-    }
-  };  
-  // Export them to import them somewhere else
-  
-  module.exports = { browse, read };
+// Declare the actions
+
+const browse = async (req, res) => {
+  const categoriesFromDB = await tables.category.readAll();
+
+  res.json(categoriesFromDB);
+};
+
+const read = (req, res) => {
+  const parsedId = parseInt(req.params.id, 10);
+
+  const category = categories.find((p) => p.id === parsedId);
+
+  if (category != null) {
+    res.json(category);
+  } else {
+    res.sendStatus(404);
+  }
+};
+// Export them to import them somewhere else
+
+module.exports = { browse, read };
--- server/app/controllers/programActions.js
+++ server/app/controllers/programActions.js
@@ -1,41 +1,35 @@
-// Some data to make the trick
+const tables = require("../../database/tables");
 
 const programs = [
     {
       id: 1,
-      title: "The Good Place",
-      synopsis:
-        "À sa mort, Eleanor Shellstrop est envoyée au Bon Endroit, un paradis fantaisiste réservé aux individus exceptionnellement bienveillants. Or Eleanor n'est pas exactement une « bonne personne » et comprend vite qu'il y a eu erreur sur la personne. Avec l'aide de Chidi, sa prétendue âme sœur dans l'au-delà, la jeune femme est bien décidée à se redécouvrir.",
-      poster:
+      title: "Desperate Houswives",
+      synopsis:"ouin ouin",
+       poster:
         "https://img.betaseries.com/JwRqyGD3f9KvO_OlfIXHZUA3Ypw=/600x900/smart/https%3A%2F%2Fpictures.betaseries.com%2Ffonds%2Fposter%2F94857341d71c795c69b9e5b23c4bf3e7.jpg",
       country: "USA",
-      year: 2016,
+      year: 2007,
+      category_id: 3,
     },
     {
       id: 2,
-      title: "Dark",
-      synopsis:
-        "Quatre familles affolées par la disparition d'un enfant cherchent des réponses et tombent sur un mystère impliquant trois générations qui finit de les déstabiliser.",
+      title: "American Horror Story",
+      synopsis: "ca fé peur",
       poster:
         "https://img.betaseries.com/zDxfeFudy3HWjxa6J8QIED9iaVw=/600x900/smart/https%3A%2F%2Fpictures.betaseries.com%2Ffonds%2Fposter%2Fc47135385da176a87d0dd9177c5f6a41.jpg",
-      country: "Allemagne",
-      year: 2017,
+      country: "USA",
+      year: 2015,
+      category_id:2,
     },
   ];
   
 // Declare the action
 
-const browse = (req, res) => {
-    if (req.query.q != null) {
-      const filteredPrograms = programs.filter((program) =>
-        program.synopsis.includes(req.query.q)
-      );
-  
-      res.json(filteredPrograms);
-    } else {
-      res.json(programs);
-    }
-  };
+const browse = async (req, res) => {
+  const programsFromDB = await tables.program.readAll();
+
+  res.json(programsFromDB);
+};
 
   const read = (req, res) => {
     const parsedId = parseInt(req.params.id, 10);
