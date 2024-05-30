0a2caf6369530defa26ec49c31c5a3213681915a
maximmft <mmaufront@gmail.com>
2024-05-30 10:19:24 +0200

    fixed bug

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,33 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+-- Create table for users
+CREATE TABLE user (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+-- Create table for items
+CREATE TABLE item (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user(id)
 );
+
+-- Create table for categories
+CREATE TABLE category (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+-- Create table for programs
+CREATE TABLE program (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  synopsis TEXT NOT NULL,
+  poster VARCHAR(255) NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  year INT UNSIGNED NOT NULL,
+  category_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
